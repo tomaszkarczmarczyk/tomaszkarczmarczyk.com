d1cc22eacdf345b4c630a75138be43a13a7f56c5
Tomasz Karczmarczyk <hello@tomaszkarczmarczyk.com>
2020-10-10 00:45:18 +0200

    Add webpack config files

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+});
