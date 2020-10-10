8a0a2000db6328e8667b7edb506dba7e0dc56b2d
Tomasz Karczmarczyk <hello@tomaszkarczmarczyk.com>
2020-10-10 23:19:27 +0200

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,8 +1,11 @@
 <?php defined('ABSPATH') or exit();
 
-function logo_url()
-{
-  $custom_logo_id = get_theme_mod('custom_logo');
-  $image = wp_get_attachment_image_src($custom_logo_id, 'full');
-  return esc_url($image[0]);
+if (!function_exists('tokk_logo_url')) {
+  function tokk_logo_url()
+  {
+    $custom_logo_id = get_theme_mod('custom_logo');
+    $image = wp_get_attachment_image_src($custom_logo_id, 'full');
+
+    return $image[0];
+  }
 }
