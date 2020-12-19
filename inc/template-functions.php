3265e9301155a1ff01f5a24e24838cd0e69caa45
Tomasz Karczmarczyk <hello@tomaszkarczmarczyk.com>
2020-12-19 23:52:09 +0100

    Update template-functions.php

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -3,6 +3,7 @@
 if (!function_exists('tokk_body_classes')) {
   function tokk_body_classes($classes)
   {
+    global $wp_query;
     $classes = ['body'];
 
     if (is_front_page()) {
@@ -13,8 +14,23 @@ if (!function_exists('tokk_body_classes')) {
       $classes[] = 'body--is-blog';
     }
 
+    if (is_category()) {
+      $classes[] = 'body--is-category';
+    }
+
     if (is_privacy_policy()) {
       $classes[] = 'body--is-privacy-policy';
+    }
+
+    if (is_singular()) {
+      $classes[] = 'body--is-singular';
+    }
+
+    if (is_single()) {
+      $post_id = $wp_query->get_queried_object_id();
+      $post = $wp_query->get_queried_object();
+      $post_type = $post->post_type;
+      $classes[] = 'body--is-single-' . sanitize_html_class($post->post_type, $post_id);
     }
 
     if (is_page()) {
@@ -23,6 +39,12 @@ if (!function_exists('tokk_body_classes')) {
 
     if (is_404()) {
       $classes[] = 'body--is-404';
+    }
+
+    if (wp_is_mobile()) {
+      $classes[] = 'body--is-mobile';
+    } else {
+      $classes[] = 'body--is-desktop';
     }
 
     return $classes;
