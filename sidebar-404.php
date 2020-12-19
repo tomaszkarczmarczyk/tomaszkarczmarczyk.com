0325167b07e81cb094c37751955f0d45226902b6
Tomasz Karczmarczyk <hello@tomaszkarczmarczyk.com>
2020-12-19 21:00:53 +0100

    Add sidebars

---
+++ sidebar-404.php
@@ -0,0 +1,5 @@
+<?php defined('ABSPATH') or exit();
+
+if (is_active_sidebar('sidebar-404')):
+  dynamic_sidebar('sidebar-404');
+endif;
---
+++ sidebar-front-page.php
@@ -0,0 +1,5 @@
+<?php defined('ABSPATH') or exit();
+
+if (is_active_sidebar('sidebar-front-page')):
+  dynamic_sidebar('sidebar-front-page');
+endif;
