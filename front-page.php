002b483a75b4325dc51792f69ad0d87684ededfd
Tomasz Karczmarczyk <hello@tomaszkarczmarczyk.com>
2020-12-19 22:18:52 +0100

    Update front page

--- front-page.php
+++ front-page.php
@@ -2,4 +2,6 @@
 
 $context = Timber::context();
 $context['post'] = new Timber\Post();
+$context['posts'] = Timber::get_posts(['posts_per_page' => 3]);
+$context['sidebar'] = Timber::get_sidebar('sidebar-front-page.php');
 Timber::render('pages/front-page.twig', $context);
