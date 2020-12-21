81710b48a64572e05517bd965dd503df7e19390c
Tomasz Karczmarczyk <hello@tomaszkarczmarczyk.com>
2020-12-21 21:23:12 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,5 +2,4 @@
 
 $context = Timber::context();
 $context['sidebar'] = Timber::get_sidebar('sidebar-404.php');
-$context['description'] = get_theme_mod('page_not_found_description');
 Timber::render('pages/404.twig', $context);
